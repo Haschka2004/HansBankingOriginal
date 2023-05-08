0eb0a7d8b31c04bf58bf135f32add9d6764e8b47
Alexander Haschka <94532156+Haschka2004@users.noreply.github.com>
2023-05-08 09:57:43 +0200

    Dashboard inklusive Anmelde Fenster

---
+++ src/main/java/com/example/mazebank/Controllers/Client/DashboardController.java
@@ -0,0 +1,37 @@
+package com.example.mazebank.Controllers.Client;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+    public Text user_name;
+    public Label login_date;
+    public Label cheking_bal;
+    public Label checking_acc_num;
+    public Label savings_bal;
+    public Label savings_acc_num;
+    public Label income_lbl;
+    public Label expense_lbl;
+    public ListView transaction_listview;
+    public TextField payee_fld;
+    public TextField amount_fld;
+    public TextField message_fld;
+    public Button send_money_btn;
+
+    /**
+     * Diese Methode wird aufgerufen, wenn man das FXML-File als View aufrufen möchte.
+     * @param url
+     * @param resourceBundle
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
