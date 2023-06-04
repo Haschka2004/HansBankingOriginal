ace1ab0b60be0ee01a146a3172a55fd179293518
Alexander Haschka <94532156+Haschka2004@users.noreply.github.com>
2023-06-04 16:12:46 +0200

    Der Admin kann jz Accounts erstellen, welche in die Datenbank gespeichert werden.

--- src/main/java/com/example/mazebank/Controllers/Admin/CreateClientController.java
+++ src/main/java/com/example/mazebank/Controllers/Admin/CreateClientController.java
@@ -1,5 +1,6 @@
 package com.example.mazebank.Controllers.Admin;
 
+import com.example.mazebank.Models.Model;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -7,6 +8,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class CreateClientController implements Initializable {
@@ -21,9 +24,87 @@ public class CreateClientController implements Initializable {
     public TextField sv_amount_fld;
     public Button create_client_btn;
     public Label error_lbl;
+    private String payeeAddress;
+    private boolean createCheckingAccountFlag;
+    private boolean createSavingsAccountFlag;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        create_client_btn.setOnAction(event -> createClient());
+        pAddress_box.selectedProperty().addListener((observableValue, oldVal, newVal) -> {
+            if(newVal){
+                payeeAddress = createPayeeAddress();
+                onCreatePayeeAddress();
+            }
+        });
+        ch_acc_box.selectedProperty().addListener((observableValue, oldVal, newVal) -> {
+          if(newVal){
+              createCheckingAccountFlag = true;
+          }
+        });
+        sv_acc_box.selectedProperty().addListener((observableValue, oldVal, newVal) -> {
+            if(newVal){
+                createSavingsAccountFlag = true;
+            }
+        });
+    }
+    public void createClient(){
+        //checking Account
+        if(createCheckingAccountFlag){
+            createAccount("Checking");
+        }
+        //savingsAccount
+        if(createSavingsAccountFlag){
+            createAccount("Savings");
+        }
+
+        //Create Client
+        String fName = fName_fld.getText();
+        String lName = lName_fld.getText();
+        String password = password_fld.getText();
+        Model.getInstance().getDatabaseDriver().createClient(fName,lName,payeeAddress,password, LocalDate.now());
+        error_lbl.setStyle("-fx-text-fill: blue; -fx-font-size: 1.3em; -fx-font-weight: bold");
+        error_lbl.setText("Client Created Successfully!");
+        emptyFields();
+
 
     }
+    private void onCreatePayeeAddress(){
+        if(fName_fld.getText() != null && lName_fld.getText() != null ){
+            pAddress_lbl.setText(payeeAddress);
+        }
+    }
+    private String createPayeeAddress(){
+        int id = Model.getInstance().getDatabaseDriver().getLastClientsId()+1;
+        char fChar = Character.toLowerCase(fName_fld.getText().charAt(0));
+        return "@"+fChar+lName_fld.getText()+id;
+    }
+
+
+    private void createAccount(String accountType){
+        double balance = Double.parseDouble(ch_amount_fld.getText());
+        // Generate Account Number
+        String firstSection = "3201";
+        String lastSection = Integer.toString((new Random()).nextInt(9999) + 1000);
+        String accountNumber = firstSection + lastSection + " " + lastSection;
+        // Erstellen des CheckingAccount oder SavingsAccount
+        if(accountType.equals("Checking")){
+            Model.getInstance().getDatabaseDriver().createCheckingAccount(payeeAddress,accountNumber,10,balance);
+        }else {
+            Model.getInstance().getDatabaseDriver().createSavingsAccount(payeeAddress,accountNumber,2000,balance);
+        }
+
+    }
+    private void emptyFields(){
+        fName_fld.setText("");
+        lName_fld.setText("");
+        password_fld.setText("");
+        pAddress_box.setSelected(false);
+        pAddress_lbl.setText("");
+        ch_acc_box.setSelected(false);
+        ch_amount_fld.setText("");
+        sv_acc_box.setSelected(false);
+        sv_amount_fld.setText("");
+    }
+
 }
